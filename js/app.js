73547bb25a2daac5ca14a7f85f8f865044df5012
Abi Klein <abi920@gmail.com>
2017-03-28 10:17:52 -0400

    deletes obsolete js folder; connects angular properly

--- js/app.js
+++
@@ -1,7 +0,0 @@
-// instantiates angular app
-var app = angular.module('quoteInspo', []);
-
-// establishes controller
-app.controller('MainController', ['$http', function($http) {
-
-}]); // end MainController
--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,8 @@
-console.log('hi');
+// instantiates angular app
+var app = angular.module("quoteInspo", []);
+
+// establishes controller
+app.controller('MainController', ['$http', function($http) {
+  this.name = 'Abi';
+  this.age = 27;
+}]); // end MainController
