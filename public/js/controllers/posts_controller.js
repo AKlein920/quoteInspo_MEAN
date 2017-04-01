fb1fe0b97a568d8625fabb7d9e7a1f6d1cb44cf9
Kaylie Weable <kaylieweable@Kaylies-MacBook-Pro.local>
2017-04-01 10:17:17 -0700

    quote populating page when its created

--- public/js/controllers/posts_controller.js
+++ public/js/controllers/posts_controller.js
@@ -6,9 +6,13 @@ function PostController($http, $state) {
     $http({
       method: 'POST',
       url: 'api/posts',
-      data: {userID: localStorage.userId, quote: this.createPost.quote, img: this.createPost.img}
+      data: {userID: localStorage.userId, quote: this.createPost.quote, img: this.createPost.img},
+      headers: { 'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('token'))}
     }).then(function(response) {
-      console.log(response.config.data);
+      // console.log(response.config.data);
+      //setting var for angular
+      this.post = response.config.data;
+      console.log(this.post);
     }.bind(this));
   };
 
