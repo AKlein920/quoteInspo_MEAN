dc77a37c15eb6003b75b1da457fe63cae074ba21
Kaylie Weable <kaylieweable@Kaylies-MacBook-Pro.local>
2017-03-31 21:09:08 -0700

    I got all the script tags linking correctly with no errors

--- public/js/controllers/posts_controller.js
+++ public/js/controllers/posts_controller.js
@@ -1,3 +1,19 @@
 function PostController($http, $state) {
   this.currentUser = localStorage.username;
+  this.createPost = {};
+
+  this.newPost = function() {
+    $http({
+      method: 'POST',
+      url: '/api',
+      data: {quote:this.createPost.quote, img: this.createPost.img}
+    }).then(function(response) {
+      console.log(response.data);
+      state.go('index');
+    });
+  };
+
+
+
+
 }
