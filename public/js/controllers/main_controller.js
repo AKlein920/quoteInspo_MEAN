dc890ad2ee13811b6f3d9174bc9f0cbb68d3ef39
Abi Klein <abi920@gmail.com>
2017-04-04 19:40:00 -0400

    adds post without page refresh

--- public/js/controllers/main_controller.js
+++ public/js/controllers/main_controller.js
@@ -1,6 +1,15 @@
 function MainController($scope, $http) {
   var self = this;
   var server = 'http://localhost:3000';
+  this.posts = [];
+  // on page load, grabs all posts from db to display even if user is not logged in
+    $http({
+      method: 'GET',
+      url: 'api/posts'
+    }).then(function(response) {
+      //this line makes it so that my array is reversed and most recent post is at the top
+      self.posts = response.data.reverse();
+  });
 
   //set current user
   $scope.$on('userLoggedIn', function(event, data){
--- public/js/controllers/posts_controller.js
+++ public/js/controllers/posts_controller.js
@@ -2,15 +2,9 @@ function PostController($scope, $http, $state, $stateParams, $rootScope) {
   var self = this;
   this.createPost = {};
   this.posts = [];
-// on page load, grabs all posts from db to display even if user is not logged in
-  $http({
-    method: 'GET',
-    url: 'api/posts'
-  }).then(function(response) {
-    //this line makes it so that my array is reversed and most recent post is at the top
-    this.posts = response.data.reverse();
-    // console.log(this.posts);
-  }.bind(this));
+  var parent = $scope.$parent.$parent.main;
+
+
 
   // $rootScope.$on('fetchData', function(event, data) {
   //   self.currentUser = data.currentUser;
@@ -19,19 +13,20 @@ function PostController($scope, $http, $state, $stateParams, $rootScope) {
 
 //create me a post
   function newPost(currentUser) {
+    // console.log($scope.$parent.$parent.main.posts);
     $http({
       method: 'POST',
       url: 'api/posts',
       data: {userId: currentUser.userId, quote: this.createPost.quote, img: this.createPost.img, date: new Date()}
     }).then(function(response) {
-      console.log(response.config.data);
+      // console.log(response.config.data);
       //setting var for angular
       this.createdPost = response.config.data;
-      this.posts.push(this.createdPost);
-      console.log(this.posts);
+      parent.posts.push(this.createdPost);
+      console.log(parent.posts.reverse());
       //this line refreshes the page so that my post shows up right when i create it.
       $state.go('index');
-    }.bind(this));
+    });
   };
 
 this.newPost = newPost;
